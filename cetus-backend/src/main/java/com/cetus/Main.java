20de8d7ccb68f2e5d3b3d04c5bbbde2633b2e0d7
Evan.Pan <yifei.pan@gingkoo.com>
2023-09-20 13:44:44 +0800

    clean files

--- cetus-backend/src/main/java/com/cetus/Main.java
+++ cetus-backend/src/main/java/com/cetus/Main.java
@@ -1,8 +1,10 @@
-package com.evan;
+package com.cetus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.cetus.dao")
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
