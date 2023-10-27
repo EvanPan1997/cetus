c2be5cc07c42f5eeceee3516f6a71b52f93db058
Evan.Pan <yifei.pan@gingkoo.com>
2023-10-27 14:45:32 +0800

    update: 增加测试案例(后续删除)

--- cetus-root/cetus-core/src/main/java/com/cetus/core/controller/CetusCoreController.java
+++ cetus-root/cetus-core/src/main/java/com/cetus/core/controller/CetusCoreController.java
@@ -1,14 +1,20 @@
 package com.cetus.core.controller;
 
+import com.cetus.core.mapper.core.SystemParamMapper;
 import com.cetus.core.util.InnerSystemParamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/api/core")
 public class CetusCoreController {
+    @Resource
+    private SystemParamMapper systemParamMapper;
+
     @GetMapping("/demo")
     public String demo() {
         return "demo";
@@ -18,5 +24,10 @@ public class CetusCoreController {
     public String getParamValue(@PathVariable("paramGroupId") String paramGroupId,
                                 @PathVariable("paramId") String paramId) {
         return InnerSystemParamUtils.getParamValue(paramGroupId, paramId);
+    }
+
+    @GetMapping("/qry")
+    public String query() {
+        return systemParamMapper.selectList(null).toString();
     }
 }
