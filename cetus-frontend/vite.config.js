e94f11eb1d7723af16f26cbaaf923796d07d323f
Evan.Pan <yifei.pan@gingkoo.com>
2023-11-03 17:38:01 +0800

    update: 前端路径访问增加前缀

--- cetus-frontend/vite.config.js
+++ cetus-frontend/vite.config.js
@@ -6,7 +6,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 8080,
+        port: 5173,
+    },
+    build: {
+        assetsInlineLimit: 0,
+        publicPath: '/cetus',
     },
     plugins: [
         vue(),
