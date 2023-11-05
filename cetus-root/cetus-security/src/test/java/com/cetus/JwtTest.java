a9a1c2bab3d7672fa52f4f28a1575c3c03e99036
Evan.Pan <yifei.pan@gingkoo.com>
2023-11-05 11:41:11 +0800

    add: JWT使用demo

---
+++ cetus-root/cetus-security/src/test/java/com/cetus/JwtTest.java
@@ -0,0 +1,37 @@
+package com.cetus;
+
+import io.jsonwebtoken.*;
+
+import java.util.Date;
+
+
+public class JwtTest {
+
+    public static void main(String[] args) {
+        String jwt = generate();
+        parse(jwt);
+    }
+
+    public static String generate() {
+        String secretKey = "Cetus_2023";
+
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .setSubject("Cetus")
+                .setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + 3600 * 1000))
+                .claim("username", "Evan");
+
+        String jwt = jwtBuilder.signWith(SignatureAlgorithm.HS256, secretKey).compact();
+        System.out.println(jwt);
+        return jwt;
+    }
+
+    public static void parse(String jwt) {
+        try {
+            Jws<Claims> jws = Jwts.parser().setSigningKey("Cetus_2023").parseClaimsJws(jwt);
+            Claims claims = jws.getBody();
+            System.out.println(claims.toString());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
